83fb72c68cc5c3d94be55711b5f87aa4d41f1187
Scott Roberts <sroberts.kia@gmail.com>
2019-09-16 22:33:50 +0000

    added exercise 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,18 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Bill", last_name: "Murray", hourly_rate: 30)
+@store1.employees.create(first_name: "Michael", last_name: "Jordan", hourly_rate: 10)
+
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,20 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee < ActiveRecord::Base
+  validates :first_name,  presence:true
+  validates :last_name, presence:true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200, message: "Must be an integer greater or equal to 40 and less than or equal to 200" }
+  belongs_to :store
+end
+
+class Store < ActiveRecord::Base
+  validates :name, length: { minimum: 3, message: "Name must be greater than 3 chars" }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: "Annual revenue must be greater than or equal to 0. Not null"}
+  has_many :employees
+end
+
+myStore = gets.chomp
+
+m = Store.create(name: myStore)
+puts m.errors.messages
